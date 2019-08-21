12c59f5bab6ef98f06e9b3b51d7dacc9733e2cc6
André Varandas <andre.m.varandas@gmail.com>
2019-08-21 17:36:43 +0100

    fix(leaderboard): removes unavailable endpoint

--- src/Client.php
+++ src/Client.php
@@ -32,7 +32,6 @@ class FortniteClient
     public $auth;
     public $challenges;
     public $items;
-    public $leaderboard;
     public $news;
     public $pve;
     public $user;
@@ -46,7 +45,6 @@ class FortniteClient
         $this->auth = new Fortnite_Auth($this);
         $this->challenges = new Fortnite_Challenges($this);
         $this->items = new Fortnite_Items($this);
-        $this->leaderboard = new Fortnite_Leaderboard($this);
         $this->news = new Fortnite_News($this);
         $this->pve = new Fortnite_PVE($this);
         $this->user = new Fortnite_User($this);
